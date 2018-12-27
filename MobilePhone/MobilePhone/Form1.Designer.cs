830b58ea287585957b67ca613c4579052c73c2ad
lab <lab@DESKTOP-JB8BGDK>
2018-12-27 18:49:56 +0300

    Добавил данные

--- MobilePhone/MobilePhone/Form1.Designer.cs
+++ MobilePhone/MobilePhone/Form1.Designer.cs
@@ -28,101 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(30, 39);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(203, 21);
-            this.comboBox1.TabIndex = 0;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(279, 39);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(205, 21);
-            this.comboBox2.TabIndex = 1;
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(279, 91);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(205, 180);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // checkedListBox1
-            // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(533, 39);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(164, 229);
-            this.checkedListBox1.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Магазин:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(276, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Производитель:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(276, 75);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Модель:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(530, 20);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Фильтр:";
-            // 
-            // MobileMagazines
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 300);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Name = "MobileMagazines";
-            this.Text = "MobileMagazines";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.comboBox2 = new System.Windows.Forms.ComboBox();
+      this.listView1 = new System.Windows.Forms.ListView();
+      this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Items.AddRange(new object[] {
+            "Связной",
+            "Теле2",
+            "Мегафон",
+            "МТС",
+            "Биллайн"});
+      this.comboBox1.Location = new System.Drawing.Point(30, 39);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(203, 21);
+      this.comboBox1.TabIndex = 0;
+      // 
+      // comboBox2
+      // 
+      this.comboBox2.FormattingEnabled = true;
+      this.comboBox2.Items.AddRange(new object[] {
+            "Apple",
+            "Samsung",
+            "Huawei",
+            "Sony",
+            "LG",
+            "Xiaomi",
+            "BBK Electronics",
+            "Nokia",
+            "HTC",
+            "Lenovo",
+            "ZTE",
+            "Motorola",
+            "ASUS"});
+      this.comboBox2.Location = new System.Drawing.Point(279, 39);
+      this.comboBox2.Name = "comboBox2";
+      this.comboBox2.Size = new System.Drawing.Size(205, 21);
+      this.comboBox2.TabIndex = 1;
+      // 
+      // listView1
+      // 
+      this.listView1.Location = new System.Drawing.Point(279, 91);
+      this.listView1.Name = "listView1";
+      this.listView1.Size = new System.Drawing.Size(205, 180);
+      this.listView1.TabIndex = 2;
+      this.listView1.UseCompatibleStateImageBehavior = false;
+      // 
+      // checkedListBox1
+      // 
+      this.checkedListBox1.FormattingEnabled = true;
+      this.checkedListBox1.Items.AddRange(new object[] {
+            "Двойная камера",
+            "Две Сим-карты",
+            "Водонепронициаемый",
+            "Противоударный",
+            "Функция NFC"});
+      this.checkedListBox1.Location = new System.Drawing.Point(533, 39);
+      this.checkedListBox1.Name = "checkedListBox1";
+      this.checkedListBox1.Size = new System.Drawing.Size(164, 229);
+      this.checkedListBox1.TabIndex = 3;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(27, 20);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(54, 13);
+      this.label1.TabIndex = 4;
+      this.label1.Text = "Магазин:";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(276, 20);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(89, 13);
+      this.label2.TabIndex = 5;
+      this.label2.Text = "Производитель:";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(276, 75);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(49, 13);
+      this.label3.TabIndex = 6;
+      this.label3.Text = "Модель:";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(530, 20);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(50, 13);
+      this.label4.TabIndex = 7;
+      this.label4.Text = "Фильтр:";
+      // 
+      // MobileMagazines
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(726, 300);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.checkedListBox1);
+      this.Controls.Add(this.listView1);
+      this.Controls.Add(this.comboBox2);
+      this.Controls.Add(this.comboBox1);
+      this.Name = "MobileMagazines";
+      this.Text = "MobileMagazines";
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
