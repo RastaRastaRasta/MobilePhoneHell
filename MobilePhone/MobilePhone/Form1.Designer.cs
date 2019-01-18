77de957c955b5b431293f909db97ea707ad7d9b5
RastaRastaRasta <zhuikoff.kirill2016@yandex.ru>
2019-01-18 14:13:21 +0400

    вывод моделей производителей(первичный)

--- MobilePhone/MobilePhone/Form1.Designer.cs
+++ MobilePhone/MobilePhone/Form1.Designer.cs
@@ -54,6 +54,7 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(205, 21);
             this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
             // 
             // listView1
             // 
--- MobilePhone/MobilePhone/Form1.cs
+++ MobilePhone/MobilePhone/Form1.cs
@@ -50,5 +50,28 @@ namespace MobilePhone
                 comboBox2.Items.AddRange(new object[] { "ZTE", "LG", "Nokia" });
             }
         }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (comboBox2.SelectedIndex == 0)
+            {
+                listView1.Items.Add("234567");
+                listView1.Items.Add("436477");
+                listView1.Items.Add("657890");
+            }
+            if (comboBox2.SelectedIndex == 1)
+            {
+                listView1.Items.Add("fdgdgd");
+                listView1.Items.Add("jumuuk");
+                listView1.Items.Add("6kbnbb");
+            }
+            if (comboBox2.SelectedIndex == 2)
+            {
+                listView1.Items.Add("234nbn");
+                listView1.Items.Add("nbnn77");
+                listView1.Items.Add("65qw90");
+            }
+        }
     }
 }
