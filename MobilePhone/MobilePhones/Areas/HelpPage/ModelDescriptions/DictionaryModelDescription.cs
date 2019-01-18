c29b6b28fd1702531cb4317d96d6d447e8defc0a
mileksa <mpa179@yandex.ru>
2019-01-19 00:07:38 +0400

    api

---
+++ MobilePhone/MobilePhones/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MobilePhones.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MobilePhone/MobilePhones/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobilePhones.WebApiApplication" Language="C#" %>
